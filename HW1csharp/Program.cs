757ff4963c02d58cdec0b3c534126df34a9147fc
Dorina <balaurdorina@gmail.com>
2022-04-21 09:56:47 +0300

    added first homework

---
+++ HW1csharp/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+
+namespace C_Sharp_Lesson_1_Homework
+{
+    public class Homework
+    {
+        public void CheckIfNumberIsEvenOrOdd(int number)
+        {
+            /* Exercise 1
+          * using "if" write a programm that checks whether an integer is greater then zero and even or odd
+          * Example:
+          * -------------------------------------------------
+          * |input   | result                               |
+          * |--------|--------------------------------------|
+          * |  1     | odd                                 |
+          * |--------|--------------------------------------|
+          * |  2     | even                                  |
+          * |--------|--------------------------------------|
+          * | -1     | the value should be greater then zero|
+          * -------------------------------------------------
+          * 
+          */
+            if (number % 2 != 0 && number > 0)
+            {
+                Console.WriteLine("odd ");
+            }
+            else if (number % 2 == 0 && number > 0)
+            {
+                Console.WriteLine("even");
+            }
+            else {
+                Console.WriteLine("The value should be greater then zero");
+            }
+        }
+        public void NumberDivideToFour(int number)
+        {
+            /* Exercise 2
+        * using a ternary operator write a program that print if number divide to 4
+        * Example: 
+        * --------------------------------------------
+        * |input   | result                          |
+        * |--------|---------------------------------|
+        * |  -4    | The number divide to 4          |
+        * |--------|---------------------------------|
+        * |   2    | The number doesn't divide to 4  |
+        * --------------------------------------------
+        */
+          
+            String isDevided = (number % 4 == 0) ? "can devide" : "cant devide";
+            Console.WriteLine(isDevided);
+            
+        }
+
+        public void DayOfWeek(string day)
+        {
+            /* Exercise 3
+             * using "switch" statement write a program that print the number of day of week
+             * Example: monday - 1, tuesday - 2 etc. 
+             * ---------------------------------------------------------
+             * |day           |number                                  |
+             * |--------------|----------------------------------------|
+             * |monday        |   1                                    |
+             * |tuesday       |   2                                    |
+             * |wednesday     |   3                                    |
+             * |thursday      |   4                                    |
+             * |friday        |   5                                    |
+             * |saturday      |   6                                    |
+             * |sunday        |   7                                    |
+             * |default value |Wrong value! Please give a day of a week|
+             * ---------------------------------------------------------
+             */
+          
+            switch (day)
+            {
+                case "Monday": 
+                    Console.WriteLine(1);
+                    break;
+                case "Tuesday":
+                    Console.WriteLine(2);
+                    break;
+                case "Wendesday":
+                    Console.WriteLine(3);
+                    break;
+                case "Thursday":
+                    Console.WriteLine(4);
+                    break;
+                case "Friday":
+                    Console.WriteLine(5);
+                    break;
+                case "Saturday":
+                    Console.WriteLine(6);
+                    break;
+                case "Sunday":
+                    Console.WriteLine(7);
+                    break;
+                default:
+                    Console.WriteLine("Wrong value! Please give a day of a week");
+                    break;
+            }
+        }
+        public void CheckLetterIfVowel(char character)
+        {
+            /* Exercise 4
+             * write a program that will print input character is a vowel, consonant or not a letter
+             * Method 1: using a switch case
+             * Method 2: using if
+             * ---------------------------
+             * |input| result            |
+             * |-----|-------------------|
+             * |  a  | a is a vowel      |
+             * |  b  | b is a consonant  |
+             * ---------------------------
+             */
+           
+            if (character == 'a' || character == 'e' || character == 'i' ||character == 'o' || character == 'u' || character == 'A' ||
+                character == 'E' || character == 'I' || character == 'O' || character == 'U') 
+            {
+                Console.WriteLine("Vowel"); 
+            }
+            else if (character == 'b' || character == 'c' || character == 'd' || character == 'f' || character == 'g' || character == 'h' ||
+                character == 'j' || character == 'k' || character == 'l' || character == 'm' || character == 'n' || character == 'p' ||
+                character == 'q' || character == 'r' || character == 's' || character == 't' || character == 'v' || character == 'w' ||
+                character == 'x' || character == 'y' || character == 'z' )
+            {
+                Console.WriteLine("Consonant");
+            }
+            else
+            {
+                Console.WriteLine("not a letter");
+            }
+
+
+        }
+        public static void Main(String[] args)
+        {
+            Homework homework = new Homework();
+
+            homework.CheckIfNumberIsEvenOrOdd(-11);
+            homework.CheckIfNumberIsEvenOrOdd(11);
+            homework.CheckIfNumberIsEvenOrOdd(8);
+            //---------------------------------------
+            homework.NumberDivideToFour(10);
+            homework.NumberDivideToFour(16);
+            //---------------------------------------
+            homework.DayOfWeek("Monday");
+            homework.DayOfWeek("Sunday");
+            homework.DayOfWeek("some day");
+            //---------------------------------------
+            homework.CheckLetterIfVowel('p');
+            homework.CheckLetterIfVowel('i');
+            
+
+        }
+    }
+}
